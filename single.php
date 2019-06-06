a6776d6f755d7ae30216cf585dc76af3a53454cd
Seshan Ravikumar <seshan10@me.com>
2019-06-06 09:25:34 -0400

    Post padding

--- single.php
+++ single.php
@@ -13,21 +13,24 @@ get_header();
         <a href="<?php echo site_url(); ?>/blog/" class="waves-effect waves-light btn"><i class="material-icons left">keyboard_backspace</i>Back to Posts</a>
         <hr/>
         <div class="card-panel white browser-default">
-            <?php
-            if ( have_posts() ) {
-                while (have_posts()) {
-                    the_post();
-                    ?>
-                    <h5> <?php the_title(); ?></h5>
-                    <h6><?php the_date(); ?></h6>
-                    <div class="">
-                        <p><?php the_content(); ?></p>
-                        <p>ID: <?php the_ID(); ?></p>
-                    </div>
-                    <?php
-                }
-            }
-            ?>
+			<div class="container">
+				<?php
+				if ( have_posts() ) {
+					while (have_posts()) {
+						the_post();
+						?>
+						<h5> <?php the_title(); ?></h5>
+						<h6><?php the_date(); ?></h6>
+						<div class="">
+							<p><?php the_content(); ?></p>
+							<p>ID: <?php the_ID(); ?></p>
+						</div>
+						<?php
+					}
+				}
+				?>
+			</div>
+           
         </div>
     </div>
 </div>
